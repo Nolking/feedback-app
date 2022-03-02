61fb13be96c39a91f5756e952fffd3d529357ef7
Nolan <nolan.nn235@gmail.com>
2022-03-02 16:28:55 +0700

    handle adding new user to mongoose

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys')
+
+const User = mongoose.model('users')
 
 passport.use(new GoogleStrategy(
     {
@@ -9,6 +12,18 @@ passport.use(new GoogleStrategy(
     callbackURL: '/auth/google/callback'
     }, 
     (accessToken, refreshToken, profile, done) => {
+        User.findOne({ googleId: profile.id }).then((existingUser) => {
+            if (existingUser) {
+                done(null, existingUser)
+            } else {
+                new User({ googleID: profile.id}).save().then(
+                    user => done(null, user)
+                )
+            } 
+        })
+
+        
+
     console.log('access token', accessToken);
     console.log('refresh token', refreshToken);
     console.log('profile', profile);

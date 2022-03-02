252c5c16ad3b73c01b78dc799e4a5e8df54a31d7
Nolan <nolan.nn235@gmail.com>
2022-03-02 11:17:42 +0700

    connect mongoose and mongodb

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
 require('./services/passport')
-const authRoutes = require('./routes/authRoutes')
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
-authRoutes(app)
+require('./routes/authRoutes')(app)
+
 app.get('/', (req, res) => {
     res.send({hi: 'there'})
 })
